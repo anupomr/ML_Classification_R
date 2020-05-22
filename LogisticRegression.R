7a5b2b413052298e2d3ae1ebcf2cab22db820260
anupo <anupomr@yahoo.com>
2020-05-22 07:47:30 -0400

    Ctrl + Shift + C to make comment and uncomment

--- LogisticRegression.R
+++ LogisticRegression.R
@@ -4,3 +4,11 @@
 dataset = read.csv('Social_Network_Ads.csv')
 dataset = dataset[, 3:5]
 
+
+# Splitting the dataset into training set and test set
+# install.packages('caTools') # Ctrl + Shift + C to make comment and uncomment
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.75)
+training_set =subset(dataset, split == TRUE)
+test_set = subset(dataset, split ==FALSE)
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ML_Classification_R
 Classification Machine Learning  with R
+
+Ctrl + Shift + C to make comment and uncomment
