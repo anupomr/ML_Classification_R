23f6af8ec09dda9a6e1e4402fa4b0f15dd77e8fb
anupo <anupomr@yahoo.com>
2020-06-05 07:53:32 -0400

    Predicting the results

--- Suppport_Vector_Machine_AR.R
+++ Suppport_Vector_Machine_AR.R
@@ -15,3 +15,17 @@ test_set = subset(dataset, split == FALSE)
 # Feature Scaling
 training_set[-3] = scale(training_set[-3])
 test_set[-3] = scale(test_set[-3])
+
+
+# Fitting K-NN to the Training set and Predicting the Test set results
+library(e1071)
+classifier = svm(formula = Purchased ~ .,
+                 data = training_set,
+                 type = 'C-classification',
+                 kernel = 'linear')
+
+# Predicting the test set results
+y_pred = predict(classifier, newdata = test_set[-3])
+
+# Making the confution matrix
+cm = table(test_set[, 3], y_pred)
